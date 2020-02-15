e1f1ee0824c9f6ee6cc12668e0fb682834676619
storykim <donghwa.s.kim@gmail.com>
2020-02-15 10:33:23 +0100

    Fix typo in intro and comment about issue page

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,8 +4,9 @@ class IntroPanel extends React.Component {
   render() {
     return (
       <div>
-        <h1>Bill Spliter</h1>
-        <p>This is bill spliter written by Donghwa Kim.</p>
+        <h1>Bill Splitter</h1>
+        <p>This is a bill splitter written by Donghwa Kim.</p>
+        <p>If you find any issues, please visit <a href="https://github.com/storykim/bill-splitter/issues">https://github.com/storykim/bill-splitter/issues.</a></p>
       </div>
     )
   }
